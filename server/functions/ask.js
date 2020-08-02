9c26e542487172905e051a4983bd95f6f05fb19e
Pouya <pouya.mozafar@gmail.com>
2020-08-02 20:07:27 +0430

    Hallo!!!!

--- server/functions/ask.js
+++ server/functions/ask.js
@@ -3,11 +3,9 @@ const { client, q } = require('../services/db');
 exports.handler = async function(event, context, callback) {
   const { queryStringParameters: { e } } = event;
   
-  return client.query(q.Match(q.Index("email"), e), {
-    data: {
-      email: e
-    }
-  }).then((e)=>{
+  return client.query(q.Get(
+    q.Match(q.Index('email_1'), e)
+  )).then((e)=>{
     return callback(null, {
       statusCode: 200,
       body: JSON.stringify({
