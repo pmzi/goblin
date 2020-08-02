931857617c36f0925cefa85eefaf3315eecff170
Pouya <pouya.mozafar@gmail.com>
2020-08-02 19:49:41 +0430

    Query is now doing its job!

--- server/functions/ask.js
+++ server/functions/ask.js
@@ -3,7 +3,11 @@ const { client, q } = require('../services/db');
 exports.handler = async function(event, context, callback) {
   const { queryStringParameters: { e } } = event;
   
-  return client.query(q.Get(q.Collection(`classes/emails/${e}`))).then((e)=>{
+  return client.query(q.Match(q.Index("email"), e), {
+    data: {
+      email: e
+    }
+  }).then((e)=>{
     return callback(null, {
       statusCode: 200,
       body: JSON.stringify({
